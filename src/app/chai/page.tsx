2224b0f8a131b2bccfc19c4b33bb7b49a5036266
ishaansahu515 <ishaansahu515@gmail.com>
2025-05-17 01:50:44 +0530

    button practice

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export default function dashboard() {
+    return (
+        <main className="h-full flex  justify-center items-center border-2">
+        <div>
+            <Button>Dashboard</Button>
+        </div>
+        </main>
+    );
+}
